dcc309154bae5772f68dd78246f4543705c2b867
duongcscx <duongcscx@gmail.com>
2022-07-28 16:34:25 +0700

    fix(CI/CD): Fix ENV

--- database/config/config.js
+++ database/config/config.js
@@ -12,6 +12,18 @@ module.exports = {
     // },
   },
 
+  test: {
+    url: `postgres://${process.env.DB_USERNAME}:${encodeURIComponent(process.env.DB_PASSWORD)}@${process.env.DB_HOST}:${
+      process.env.DB_PORT
+    }/${process.env.DB_NAME}`,
+    dialect: 'postgres',
+    // ssl: true,
+    // dialectOptions: {
+    //   ssl: {
+    //     require: true,
+    //   },
+    // },
+  },
   production: {
     url: `postgres://${process.env.DB_USERNAME}:${encodeURIComponent(process.env.DB_PASSWORD)}@${process.env.DB_HOST}:${
       process.env.DB_PORT
